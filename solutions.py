4685766f257d8ad229b2fdc23bd862944251b228
shenmemingzine <rongwingyu@gmail.com>
2018-06-24 18:42:06 -0700

    Update solutions.py

--- solutions.py
+++ solutions.py
@@ -162,6 +162,31 @@ def solution(string, markers):
         lines[i] = line.rstrip()
     return "\n".join(lines)
     
-# 
+# John and Ann sign up for Codewars
+def kata_num(n):
+    ann = [1]*n
+    john = [0]*n
+    day = 1
+    while day < n:
+        ann[day] = day - john[ann[day - 1]]
+        john[day] = day - ann[john[day - 1]]
+        day += 1
+    return ann, john
+
+def john(n):
+    return kata_num(n)[1]
+    
+def ann(n):
+    return kata_num(n)[0]
+    
+def sum_john(n):
+    return sum(john(n))
+    
+def sum_ann(n):
+    return sum(ann(n))
+    
+    
+print(sum_john(75))
+print(sum_ann(115))
 
 #
