cc95d8643453ec84b9fe8d02520a10f2993476ae
shenmemingzine <rongwingyu@gmail.com>
2018-07-07 15:31:31 -0700

    Update Molecule to atoms.py

--- Molecule to atoms.py
+++ Molecule to atoms.py
@@ -8,24 +8,44 @@ def parse_molecule(formula):
     formula = re.sub("[\]\}]", ")", formula)
     
     idx = 0
-    elements = []
-
+    elements = {}
+    left_parenthese = []
+    
     while idx < formula_length:
         if formula[idx].isupper():
             if formula[idx] not in elements:
-                elements.append(formula[idx])
-                idx += 1 
+                if idx + 1 < formula_length and formula[idx + 1].isdigit():
+                    elements[formula[idx]] = int(formula[idx + 1])
+                    idx += 2
+                else:
+                    elements[formula[idx]] = 1
+                    idx += 1
             else:
-                idx += 1
+                if idx + 1 < formula_length and formula[idx + 1].isdigit():
+                    elements[formula[idx]] += int(formula[idx + 1])
+                    idx += 2
+                else:
+                    elements[formula[idx]] += 1
+                    idx += 1
         elif formula[idx].islower():
-            elements[-1] = formula[idx - 1:idx + 1]
+            elements[formula[idx - 1:idx + 1]] = elements[formula[idx - 1]]
+            del elements[formula[idx - 1]]
             idx += 1
         elif formula[idx].isdigit():
             idx += 1 
-        elif formula[idx] == "(" or formula[idx] == ")" :
+        elif formula[idx] == "(":
+            left_parenthese.append(idx)
             idx += 1 
+        elif formula[idx] == ")":
+            if idx + 1 < formula_length and formula[idx + 1].isdigit():
+                for e in elements:
+                    if e in formula[left_parenthese[-1] + 1:idx]:
+                        elements[e] *= int(formula[idx + 1])
+                left_parenthese.remove(left_parenthese[-1])
+                # print(formula[left_parenthese[-1] + 1:idx])
+            idx += 1    
     return elements
     
-print(parse_molecule("H2O"))
+print(parse_molecule("H2OO"))
 print(parse_molecule("Mg(OH)2"))
 print(parse_molecule("K4[ON(SO3)2]2"))
