19094b20a5b2297cbbb46dc23eed33198ca83283
shenmemingzine <rongwingyu@gmail.com>
2018-07-18 14:24:35 -0700

    Create SQL Basics: Simple PIVOTING data WITHOUT CROSSTAB.sql

---
+++ SQL/SQL Basics: Simple PIVOTING data WITHOUT CROSSTAB.sql
@@ -0,0 +1,8 @@
+select p.name,
+  count(d.id) filter (where d.detail = 'good') as good,
+  count(d.id) filter (where d.detail = 'ok') as ok,
+  count(d.id) filter (where d.detail = 'bad') as bad
+from products p
+join details d on p.id = d.product_id
+group by p.name
+order by p.name
